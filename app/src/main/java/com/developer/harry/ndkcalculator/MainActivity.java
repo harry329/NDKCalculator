949be2a8c245ba43c3ef956465abef7ae7c28f81
Harsimran <harsimrans329@gmail.com>
2016-04-25 21:03:08 -0500

    first commit

---
+++ app/src/main/java/com/developer/harry/ndkcalculator/MainActivity.java
@@ -0,0 +1,69 @@
+package com.developer.harry.ndkcalculator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mNumber1EditText;
+    EditText mNumber2EditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mNumber1EditText = (EditText)findViewById(R.id.number1);
+        mNumber2EditText =(EditText) findViewById(R.id.number2);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void operationClicked(View v){
+        if(mNumber1EditText.getText().toString().equals("")||
+                mNumber2EditText.getText().toString().equals("")){
+            Toast.makeText(MainActivity.this, "number field is empty",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String operation = ((Button) v).getText().toString();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
